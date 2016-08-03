0db710d644e1870a785f4a381e4cb81eb102e87f
baderd <daniel.magnus.bader@gmail.com>
2016-08-03 14:00:31 +0200

    smaller html

--- dataviz_d3js/mini_project1_raw_visualization.R
+++ dataviz_d3js/mini_project1_raw_visualization.R
@@ -23,14 +23,15 @@ below_1year = c('0-6 days','7-27 days','28-364 days')
 colpal = colorpalette('colorblind', length(some_countries))
 
 # define plot table
-dtplot = dt[Sex=='Both' & Age_Group %in% below_1year & Country_Name %in% some_countries]
-dtplot[,death_ratio_per_100k:= mean(Death_Rate_Per_100000), by=c('Year', 'Country_Code')]
+dtplot = dt[Sex=='Both' & Age_Group %in% below_1year & Country_Name %in% some_countries,
+       .(death_ratio_per_100k= mean(Death_Rate_Per_100000)), 
+       by=c('Year', 'Country_Name')
+       ]
 
 # plot
 plot_ly(x= Year, y= death_ratio_per_100k, 
         data= dtplot, 
-        color=Country_Name, colors=colpal,
-        name= Country_Name
+        color=Country_Name, colors=colpal
         )
 layout(title= 'Mortality drops for children <br>younger than 1 year across all continents', 
        yaxis = list(exponentformat = "B", type='linear')
