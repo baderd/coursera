bd82abd750a6c3c40e11a802afd0834ca0f57990
Daniel M Bader <daniel.magnus.bader@gmail.com>
2017-11-01 19:27:30 +0100

    lecture notes

---
+++ matlab_intro/crazy_matlab.m
@@ -0,0 +1,33 @@
+% 
+% Matlab features, I would not think of
+%
+
+% ellipsis for multiline commands
+long_sum = sum([ 1 ...
+    2 ...
+    3 ...
+    ]);
+
+% max() returns both maximum and its index if needed
+[maxi, idx_maxi] = max([1 10 2 3]);
+
+% also size() results can be assigned to two scalars
+[rows, cols] = size(rand(2,3));
+
+% diag() creates a matrix with given input on diagonal
+dd = diag([1 3 56 9]);
+
+% restart random number generator
+rng(0);
+rr = rand;
+rng(0);
+rr - rand;
+
+% basic plot and legend
+plot(1:2, rand(2));
+legend('a','b');
+
+
+
+
+
