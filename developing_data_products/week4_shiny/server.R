bb82f512a24847b14f00a03fef9fb0f33d1e7a61
Daniel M Bader <daniel.magnus.bader@gmail.com>
2019-10-02 11:06:35 +0200

    made plot dynamic to table changes, inserted docu, build report based on tmp file saved table

--- developing_data_products/week4_shiny/server.R
+++ developing_data_products/week4_shiny/server.R
@@ -22,28 +22,43 @@ shinyServer(function(input, output) {
         }), 
         mtcars
     )
+    rownames(df_cars_raw) <- NULL
+    
+    # subset by search term
     dfcars <- reactive({
         subset(df_cars_raw, grepl(input$car_search, car_name, ignore.case = T))
     })
     
-    output$hp_vs_qsec <- renderPlotly({
-        plot_ly(data = dfcars(), x = ~hp, y = ~qsec, hovertext = ~car_name) %>%
-            add_markers()
-    })
-    
+    # display editable table
     tmp_hot <- reactive({
         rhandsontable(data = dfcars(), rowHeaders = NULL) %>%
             hot_table(highlightRow = TRUE) %>%
             hot_cols(columnSorting = TRUE)
     })
     output$hot <- renderRHandsontable(tmp_hot())
-    # output$simple_tab <- renderTable({head(hot_to_r(input$hot))})
     
+    
+    # plot horsepower vs quarter mile seconds
+    output$hp_vs_qsec <- renderPlotly({
+        shiny::req(input$hot)
+        plot_ly(
+            data = hot_to_r(input$hot),
+            x = ~hp, y = ~qsec, hovertext = ~car_name
+        ) %>%
+            add_markers()
+    })
+    
+    
+    # generate downloadable report with current modified table
     output$report <- downloadHandler(
         filename = "report.html",
         content = function(file) {
+            # save hot to tmp file
+            tmpfile <- tempfile(fileext = ".csv")
+            write.csv(x = hot_to_r(input$hot), file = tmpfile)
+            
             # Set up parameters to pass to Rmd document
-            params <- list(hot = input$hot, ptly = input$hp_vs_qsec)
+            params <- list(file_table = tmpfile)
             
             # Knit the document, passing in the `params` list, and eval it in a
             # child of the global environment (this isolates the code in the document
--- developing_data_products/week4_shiny/ui.R
+++ developing_data_products/week4_shiny/ui.R
@@ -18,16 +18,36 @@ shinyUI(fluidPage(
     # Sidebar with a filter inputs
     sidebarLayout(
         sidebarPanel(
-            downloadButton("report", "Generate report"),
+            width = 3,
+            h2("Car search"),
+            p("(regex allowed)"),
             textInput(
                 inputId = "car_search", 
                 label = "Search for cars by name", 
                 value = ""
-            )
+            ),
+            h2("Report"),
+            p("When you are done modifying the table, 
+               click here to download a standalone HTML report:"),
+            downloadButton(outputId = "report", label = "Generate report")
         ),
 
         # Show the mtcars table
         mainPanel(
+            h2("Documentation"),
+            HTML("This app allows you to subset the mtcars table by car name and 
+              plot the corresponding horse power (hp) versus the time in seconds 
+              the car needs for a quarter mile (qsec). Furthermore, the table 
+              displayed is editable (like excel), so you modify your favorite 
+              car to be the fastest and most powerful. The plot will change 
+              automatically.  
+              <br><br>
+              The <b>mtcars</b> table is a R built-in data set. 
+              \"The data was extracted from the 1974 Motor Trend US magazine, 
+              and comprises fuel consumption and 10 aspects of automobile design 
+              and performance for 32 automobiles (1973–74 models).\"
+              "),
+            HTML("<br><br>"),
             plotlyOutput("hp_vs_qsec"),
             # tableOutput("simple_tab"),
             rHandsontableOutput("hot")
